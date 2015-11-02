296c7790761b5d52a11ab691bd16ff19e1c02fce
Ann <anna.chumak1409@gmail.com>
2015-11-02 21:37:58 +0200

    idea project

---
+++ Lessons/Lesson_4/src/main/java/Lesson4.java
@@ -0,0 +1,37 @@
+
+public class Lesson4 {
+
+    public static void main(String[] args){
+
+        int a = 5;//int x = 0; int = x, y, z; - this format is not recommended
+        int b = 9;
+        int x=0x41;
+        double t = 9;
+        int r = 2;
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println((char)x);//явное преобразование(приведение) типов
+        System.out.println(t/r);
+
+
+        double c = 938;
+        int d = 246;
+        int z = 2;
+        int k =(int)((c / d)*Math.pow(10,z+1));
+        int zn = (int)(k-(((int)(k/10))*10));
+        System.out.println(zn);
+
+        if (zn>=5){
+            System.out.println((double)(((int)(k/10))+1) / (double)Math.pow(10.0,z));
+        }else{
+            System.out.println((double)(((int)(k/10))) / (double)Math.pow(10.0,z));
+        }
+
+
+
+
+
+    }
+}
