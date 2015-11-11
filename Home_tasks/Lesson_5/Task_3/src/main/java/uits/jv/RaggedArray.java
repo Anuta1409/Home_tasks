bb3260437b1eb1346b684695b6cc8938241e1826
Ann <anna.chumak1409@gmail.com>
2015-11-11 23:01:16 +0200

    Hometask lesson 5. Massive and massive sort

---
+++ Home_tasks/Lesson_5/Task_3/src/main/java/uits/jv/RaggedArray.java
@@ -0,0 +1,22 @@
+package uits.jv;
+
+/**
+ * Created by Anna on 11.11.2015.
+ */
+public class RaggedArray {
+    public static void main(String[] args) {
+
+        int[][] mas = new int[10][];
+
+        for(int i=0; i < mas.length; i++){
+            mas[i] = new int[mas.length-i];
+        }
+
+        for(int i = 0; i < mas.length; i++){
+            System.out.println( );
+            for(int j = 0; j < mas[i].length; j++){
+                System.out.print(i+" , "+j);
+            }
+        }
+    }
+}
