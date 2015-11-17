751e8c1484592682cb7e70536870dd3c1bbf4c62
Ann <anna.chumak1409@gmail.com>
2015-11-17 21:23:45 +0200

    task 3

--- Home_tasks/Lesson_5/Task_3/src/main/java/uits/jv/RaggedArray.java
+++ Home_tasks/Lesson_5/Task_3/src/main/java/uits/jv/RaggedArray.java
@@ -8,15 +8,22 @@ public class RaggedArray {
 
         int[][] mas = new int[10][];
 
-        for(int i=0; i < mas.length; i++){
-            mas[i] = new int[i+1];
+        for (int i = 0; i < mas.length; i++) {
+           // mas[i] = new int[i];//прямой угол слева
+            mas[i] = new int[mas.length - i];//вверх ногами
         }
 
-        for(int i = 0; i < mas.length; i++){
-            System.out.println( );
-            for(int j = 0; j < mas[i].length; j++){
-                System.out.print(i+" , "+j);
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas[i].length; j++) {
+                if (j == 0) {
+                    for (int t = 0; t < mas.length - mas[i].length; t++) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.print(mas[i][j]+"  ");
             }
+            System.out.println();
         }
     }
 }
+
