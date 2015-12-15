40bc5420fa666ac2cd7fde23188526a04d110a1d
Ann <anna.chumak1409@gmail.com>
2015-12-15 22:11:49 +0200

    calculator build 2

--- Home_tasks/Calculator/src/main/java/calculator/App.java
+++ Home_tasks/Calculator/src/main/java/calculator/App.java
@@ -19,30 +19,24 @@ public class App {
         System.out.println("3 : *");
         System.out.println("4 : /");
 
-        if(sc.hasNextInt()){
-            a = sc.nextInt();
-        }else{
-            System.out.println("Sorry you add not valid data. Try again!");
-        }
 
-        System.out.println("Add please first number:");
-        if(sc.hasNextDouble()){
-            b = sc.nextDouble();
-        }else{
-            System.out.println("Sorry you add not valid data. Try again!");
-        }
-
-
-        System.out.println("Add please second number:");
-        if(sc.hasNextDouble()){
-            c = sc.nextDouble();
-        }else{
-            System.out.println("Sorry you add not valid data. Try again!");
-        }
-
-        System.out.println();
-        MathActions res = new MathActions();
-        res.MathOp(a,b,c);
+            try {
+                a = sc.nextInt();
+                System.out.println("Add please first number:");
+                b = sc.nextDouble();
+                System.out.println("Add please second number:");
+                c = sc.nextDouble();
+                System.out.println();
+                MathActions res = new MathActions();
+                res.MathOp(a,b,c);
+            }catch(Exception e){
+                System.out.println("Sorry you add not valid data. Try again!");
+            }
+//        try {
+//            if((a == 0) && (a > 4));
+//        } catch (Exception e){
+//            System.out.println("wwwwwwwwwwww");
+//        }
     }
 
 }
